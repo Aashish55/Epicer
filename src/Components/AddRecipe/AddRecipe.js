8aff68d27729e23105a8a5ec57164bb8ac68afb0
Aashish Jangam <aashish.jangam55@gmail.com>
2021-01-31 20:24:18 +0545

    form validation

--- src/Components/AddRecipe/AddRecipe.js
+++ src/Components/AddRecipe/AddRecipe.js
@@ -3,7 +3,15 @@ import './AddRecipe.css'
 
 const AddRecipe = () => {
 
+    const [recipeName, setRecipeName] = useState('');
+    const [lists, setLists] = useState('')
     const [ingredients, setIngredients] = useState([{ ingredient: '', quantity: '' }]);
+    const [unit, setUnit] = useState('')
+    const [imageURL, setImageURL] = useState('')
+    const [description, setDescription] = useState('')
+    const [authorName, setAuthorName] = useState('')
+    const [error, setError] = useState('')
+
 
     const handleInputChange = (e, index) => {
         const newSetOfIngredients = [...ingredients];
@@ -28,6 +36,23 @@ const AddRecipe = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(ingredients);
+        if (formvalid()) {
+            console.log('Ready to save');
+        }
+    }
+
+    const formvalid = () => {
+        if (isFormEmpty()) {
+            const errorMessage = { message: "Fill in all fields" };
+            setError([...error, errorMessage])
+            //   this.setState({ errors: errors.concat(error) });
+            return false;
+        } else {
+            return true;
+        }
+    }
+    const isFormEmpty = () => {
+        return !recipeName.length || !lists.length || !unit.length || !imageURL.length || !authorName.length || !description.length || !ingredients[0].ingredient || !ingredients[0].quantity;
     }
 
     return (
@@ -37,14 +62,18 @@ const AddRecipe = () => {
                 <h3 className='formTitle'>Add Recipe</h3>
                 <input
                     className="addInput"
-                    name="title"
+                    name="recipeName"
                     type="text"
+                    value={recipeName}
+                    onChange={(e) => setRecipeName(e.target.value)}
                     placeholder='Recipe Name'
                 />
                 <input
                     className="addInput"
                     name="lists"
                     type="text"
+                    value={lists}
+                    onChange={(e) => setLists(e.target.value)}
                     placeholder='List of Ingredients. e.g: butter, flour etc'
                 />
                 {
@@ -79,19 +108,33 @@ const AddRecipe = () => {
                     className="addInput"
                     name="unit"
                     type="text"
+                    value={unit}
+                    onChange={(e) => setUnit(e.target.value)}
                     placeholder='Units of the quantities'
                 />
                 <input
                     className="addInput"
                     name="imageURL"
                     type="text"
+                    value={imageURL}
+                    onChange={(e) => setImageURL(e.target.value)}
                     placeholder='URL/link of the food,images'
                 />
                 <input
                     className="addInput large"
                     name="description"
                     type="text"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
                     placeholder='Steps to Cook'
+                />
+                <input
+                    className="addInput"
+                    name="authorName"
+                    type="text"
+                    value={authorName}
+                    onChange={(e) => setAuthorName(e.target.value)}
+                    placeholder='Author Name'
                 />
                 <button className="submitForm" onClick={handleSubmit} >
                     Save Recipe
