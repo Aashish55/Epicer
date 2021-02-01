9c70d98741f8ce3a6dde0f0f0906e9fa91f9021e
Aashish Jangam <aashish.jangam55@gmail.com>
2021-02-01 12:44:05 +0545

    dynamic homepage

--- src/Components/AddRecipe/AddRecipe.js
+++ src/Components/AddRecipe/AddRecipe.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './AddRecipe.css'
+import { Link } from "react-router-dom";
 
 const AddRecipe = () => {
 
@@ -10,7 +11,9 @@ const AddRecipe = () => {
     const [imageURL, setImageURL] = useState('')
     const [description, setDescription] = useState('')
     const [authorName, setAuthorName] = useState('')
-    const [error, setError] = useState('')
+    const [errors, setErrors] = useState([]);
+
+    const [recipeStore, setRecipeStore] = useState([]);
 
 
     const handleInputChange = (e, index) => {
@@ -35,17 +38,47 @@ const AddRecipe = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(ingredients);
         if (formvalid()) {
             console.log('Ready to save');
+            console.log(recipeStore)
+            const recipeData = {
+                recipeName: recipeName,
+                ingredientsList: lists,
+                ingredients: ingredients,
+                unit: unit,
+                imageURL: imageURL,
+                description: description,
+                authorName: authorName
+            };
+            const newStore = [...recipeStore];
+            newStore.push(recipeData);
+            setRecipeStore(newStore)
+            //setRecipeStore(oldStore => [...oldStore, { recipeData }])
+            console.log(recipeStore);
+            localStorage.setItem('recipe', JSON.stringify(newStore));
+            clearInputFields();
         }
+    }
+
+    const clearInputFields = () => {
+        setRecipeName('');
+        setLists('');
+        setUnit('');
+        setImageURL('');
+        setAuthorName('');
+        setDescription('');
+        setIngredients([{ ingredient: '', quantity: '' }]);
     }
 
     const formvalid = () => {
         if (isFormEmpty()) {
             const errorMessage = { message: "Fill in all fields" };
-            setError([...error, errorMessage])
-            //   this.setState({ errors: errors.concat(error) });
+
+            const newMesage = [...errors];
+            newMesage.push(errorMessage);
+            setErrors(newMesage)
+            //setErrors([...errors, errorMessage])
+            console.log(errors)
             return false;
         } else {
             return true;
@@ -55,10 +88,21 @@ const AddRecipe = () => {
         return !recipeName.length || !lists.length || !unit.length || !imageURL.length || !authorName.length || !description.length || !ingredients[0].ingredient || !ingredients[0].quantity;
     }
 
+
+    useEffect(() => {
+        const item = JSON.parse(localStorage.getItem('recipe'));
+        if (item) {
+            setRecipeStore(item)
+        }
+    }, [])
+
     return (
         <div className='addRecipe'>
             <h2 className='title'><span>Burgeon</span> Recipe</h2>
-            <form className='form'>
+            <button className="addMoreButton">
+                <Link className='link' to="/">Back to Homepage</Link>
+            </button>
+            <div className='form'>
                 <h3 className='formTitle'>Add Recipe</h3>
                 <input
                     className="addInput"
@@ -85,7 +129,7 @@ const AddRecipe = () => {
                                 className="addInput"
                                 name="ingredient"
                                 type="text"
-                                placeholder={`Name of Ingredient`}
+                                placeholder={`${index + 1}. Name of Ingredient`}
                             />
                             <input
                                 value={quantity}
@@ -93,7 +137,8 @@ const AddRecipe = () => {
                                 className="addInput"
                                 name="quantity"
                                 type="number"
-                                placeholder={`Quantity`}
+                                min="1"
+                                placeholder={`${index + 1}. Quantity`}
                             />
                             {ingredients.length > 1 ? <button className="delete" onClick={(e) => deleteIngredient(e, index)} >
                                 X
@@ -139,7 +184,7 @@ const AddRecipe = () => {
                 <button className="submitForm" onClick={handleSubmit} >
                     Save Recipe
                 </button>
-            </form>
+            </div>
         </div>
     );
 }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,76 +1,81 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Home.css'
+import { Link } from "react-router-dom";
 
 const Home = () => {
 
-    const loginHandler = () => {
-        console.log("On submit");
-    }
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        const item = JSON.parse(localStorage.getItem('recipe'));
+        if (item) {
+            setRecipes(item)
+        }
+        console.log(item)
+    }, [])
 
     return (
         <div className='home'>
             <h2 className='title'><span>Burgeon</span> Recipe</h2>
-            <div className='searchArea'>
-                <input
-                    className="search"
-                    name="search"
-                    type="text"
-                    placeholder='Serach here...'
-                />
-                <button className="submit" onClick={loginHandler}>
-                    Search
+            <div className='field-area'>
+                <div>
+                    <div className='searchArea'>
+                        <input
+                            className="search"
+                            name="search"
+                            type="text"
+                            placeholder='Serach here...'
+                        />
+                        <button className="submit">
+                            Search
+                        </button>
+                    </div>
+                </div>
+                <button className="addMoreButton">
+                    <Link className='link' to="/add">Add More Recipe</Link>
                 </button>
             </div>
 
-            <div className='recipeCard'>
-                <img className='recipeImage' src='https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' alt='food1' />
-                <div className='textArea'>
-                    <h3 className='recipeTitle'>Falful</h3>
+            <div className='flex-wrap'>
+                {
+                    recipes.map((recipe, index) => (
+                        <div key={index} className='recipeCard'>
+                            <img className='recipeImage' src='https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' alt='food1' />
+                            <div className='textArea'>
+                                <h3 className='recipeTitle'>{recipe.recipeName}</h3>
 
-                    <div className='leftRightContainer'>
-                        <div className='left'>
-                            <p className='ingredientLists'>Here goes the all ingredients lists.Here goes the all ingredients lists.Here goes the all ingredients lists.Here goes the all ingredients lists.</p>
-                            <br />
-                            <p className='ingredientLists'>Here goes the all steps.Here goes the all ingredients lists.Here goes the all ingredients lists.Here goes the all ingredients lists.</p>
-                            <br />
-                            <p className='ingredientLists'>NOTE: Quantity is measured in...</p>
-                        </div>
-                        <div className='right'>
-                            <div className='row'>
-                                <h4 className='cardSubtitle'>Ingredients</h4>
-                                <h4 className='cardSubtitle'>Quantity</h4>
-                            </div>
-                            <div className='rowData'>
-                                <p className='cardSubtitleData'>Ingredients</p>
-                                <p className='cardSubtitleData'>Quantity</p>
-                            </div>
-                            <div className='rowData'>
-                                <p className='cardSubtitleData'>Ingredients</p>
-                                <p className='cardSubtitleData'>Quantity</p>
-                            </div>
-                            <div className='rowData'>
-                                <p className='cardSubtitleData'>Ingredients</p>
-                                <p className='cardSubtitleData'>Quantity</p>
-                            </div>
-                            <div className='rowData'>
-                                <p className='cardSubtitleData'>Ingredients</p>
-                                <p className='cardSubtitleData'>Quantity</p>
-                            </div>
-                            <div className='rowData'>
-                                <p className='cardSubtitleData'>Ingredients</p>
-                                <p className='cardSubtitleData'>Quantity</p>
-                            </div>
-                            <div className='rowData'>
-                                <p className='cardSubtitleData'>Ingredients</p>
-                                <p className='cardSubtitleData'>Quantity</p>
+                                <div className='leftRightContainer'>
+                                    <div className='left'>
+                                        <p className='ingredientLists'>{recipe.ingredientsList}</p>
+                                        <br />
+                                        <p className='ingredientLists'>{recipe.description}</p>
+                                        <br />
+                                        <p className='ingredientLists'>NOTE: Quantity is measured in {recipe.unit}</p>
+                                    </div>
+                                    <div className='right'>
+                                        <div className='row'>
+                                            <h4 className='cardSubtitle'>Ingredients</h4>
+                                            <h4 className='cardSubtitle'>Quantity</h4>
+                                        </div>
+                                        {
+                                            recipe.ingredients.map((ingredient, subIndex) => (
+                                                <div key={subIndex} className='rowData'>
+                                                    <p className='cardSubtitleData'>{ingredient.ingredient}</p>
+                                                    <p className='cardSubtitleData'>{ingredient.quantity}</p>
+                                                </div>
+                                            ))
+                                        }
+
+                                    </div>
+                                </div>
+
+                                <br />
+                                <br />
+                                <p className='footerText'>Added by: {recipe.authorName}</p>
                             </div>
                         </div>
-                    </div>
-
-                    <br />
-                    <br />
-                    <p className='footerText'>Added by: Aashish Jangam</p>
-                </div>
+                    ))
+                }
             </div>
         </div>
     );
