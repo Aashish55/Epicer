5d943bbfe384d277091fd2aa53c331593a68e25e
Aashish Jangam <aashish.jangam55@gmail.com>
2021-02-02 11:23:19 +0545

    filter

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,8 +6,16 @@ const Home = () => {
 
     const [recipes, setRecipes] = useState([]);
     const [searchValue, setSearchValue] = useState('');
-    const [searchResults, setSearchResults] = useState([]);
 
+
+    useEffect(() => {
+        const item = JSON.parse(localStorage.getItem('recipe'));
+        if (item) {
+            setRecipes(item)
+        }
+        console.log(item)
+
+    }, [])
 
     const validURL = (str) => {
         var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
@@ -19,25 +27,63 @@ const Home = () => {
         return !!pattern.test(str);
     }
 
-    const handleSearchIngredients = () => {
-        const recipesAcc = [...recipes];
-        const regex = new RegExp(searchValue, "gi");
-        const searchResults = recipesAcc.reduce((acc, recipe) => {
-            if (recipe.recipeName && recipe.recipeName.match(regex)) {
-                acc.push(recipe);
-            }
-            return acc;
-        }, []);
-        setSearchResults(searchResults);
-    };
 
-    useEffect(() => {
-        const item = JSON.parse(localStorage.getItem('recipe'));
-        if (item) {
-            setRecipes(item)
+    const displayRecipeCard = (recipe, index) => {
+        return (
+            <div key={index} className='recipeCard'>
+                <img className='recipeImage' src={validURL(recipe.imageURL) ? recipe.imageURL : 'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'} alt='ImageHere' />
+                <div className='textArea'>
+                    <h3 className='recipeTitle'>{recipe.recipeName}</h3>
+                    <div className='leftRightContainer'>
+                        <div className='left'>
+                            <p className='ingredientLists'>{recipe.ingredientsList}</p>
+                            <br />
+                            <p className='ingredientLists'>{recipe.description}</p>
+                            <br />
+                            <p className='ingredientLists'>NOTE: Quantity is measured in {recipe.unit}</p>
+                        </div>
+                        <div className='right'>
+                            <div className='row'>
+                                <h4 className='cardSubtitle'>Ingredients</h4>
+                                <h4 className='cardSubtitle'>Quantity</h4>
+                            </div>
+                            {
+                                recipe.ingredients.map((ingredient, subIndex) => (
+                                    <div key={subIndex} className='rowData'>
+                                        <p className='cardSubtitleData'>{ingredient.ingredient}</p>
+                                        <p className='cardSubtitleData'>{ingredient.quantity}</p>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                    <br />
+                    <br />
+                    <p className='footerText'>Added by: {recipe.authorName}</p>
+                </div>
+            </div>
+        );
+    }
+
+    const regex = new RegExp(searchValue, "gi");
+
+    const searchResults = recipes.filter((recipe) => {
+
+        if (recipe.recipeName && recipe.recipeName.match(regex)) {
+            return true;
+        } else {
+            let doesIncludes = false;
+            recipe.ingredients.forEach(({ ingredient }) => {
+                if (ingredient.match(regex)) { doesIncludes = true; }
+            })
+            if (doesIncludes) {
+                return true;
+            }
         }
-        console.log(item)
-    }, [])
+        return false;
+
+    });
+
 
     return (
         <div className='home'>
@@ -50,10 +96,9 @@ const Home = () => {
                             name="search"
                             type="text"
                             value={searchValue}
-                            onChange={(e) => {
+                            onChange={(e) =>
                                 setSearchValue(e.target.value)
-                                handleSearchIngredients()
-                            }}
+                            }
                             placeholder='Serach here...'
                         />
                         <button className="submit">
@@ -69,79 +114,10 @@ const Home = () => {
             <div className='flex-wrap'>
                 {
                     searchResults.length > 0 ? searchResults.map((recipe, index) => (
-                        <div key={index} className='recipeCard'>
-                            <img className='recipeImage' src={validURL(recipe.imageURL) ? recipe.imageURL : 'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'} alt='ImageHere' />
-                            <div className='textArea'>
-                                <h3 className='recipeTitle'>{recipe.recipeName}</h3>
-
-                                <div className='leftRightContainer'>
-                                    <div className='left'>
-                                        <p className='ingredientLists'>{recipe.ingredientsList}</p>
-                                        <br />
-                                        <p className='ingredientLists'>{recipe.description}</p>
-                                        <br />
-                                        <p className='ingredientLists'>NOTE: Quantity is measured in {recipe.unit}</p>
-                                    </div>
-                                    <div className='right'>
-                                        <div className='row'>
-                                            <h4 className='cardSubtitle'>Ingredients</h4>
-                                            <h4 className='cardSubtitle'>Quantity</h4>
-                                        </div>
-                                        {
-                                            recipe.ingredients.map((ingredient, subIndex) => (
-                                                <div key={subIndex} className='rowData'>
-                                                    <p className='cardSubtitleData'>{ingredient.ingredient}</p>
-                                                    <p className='cardSubtitleData'>{ingredient.quantity}</p>
-                                                </div>
-                                            ))
-                                        }
-
-                                    </div>
-                                </div>
-
-                                <br />
-                                <br />
-                                <p className='footerText'>Added by: {recipe.authorName}</p>
-                            </div>
-                        </div>
-                    )
+                        displayRecipeCard(recipe, index))
                     ) :
                         recipes.map((recipe, index) => (
-                            <div key={index} className='recipeCard'>
-                                <img className='recipeImage' src={validURL(recipe.imageURL) ? recipe.imageURL : 'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'} alt='ImageHere' />
-                                <div className='textArea'>
-                                    <h3 className='recipeTitle'>{recipe.recipeName}</h3>
-
-                                    <div className='leftRightContainer'>
-                                        <div className='left'>
-                                            <p className='ingredientLists'>{recipe.ingredientsList}</p>
-                                            <br />
-                                            <p className='ingredientLists'>{recipe.description}</p>
-                                            <br />
-                                            <p className='ingredientLists'>NOTE: Quantity is measured in {recipe.unit}</p>
-                                        </div>
-                                        <div className='right'>
-                                            <div className='row'>
-                                                <h4 className='cardSubtitle'>Ingredients</h4>
-                                                <h4 className='cardSubtitle'>Quantity</h4>
-                                            </div>
-                                            {
-                                                recipe.ingredients.map((ingredient, subIndex) => (
-                                                    <div key={subIndex} className='rowData'>
-                                                        <p className='cardSubtitleData'>{ingredient.ingredient}</p>
-                                                        <p className='cardSubtitleData'>{ingredient.quantity}</p>
-                                                    </div>
-                                                ))
-                                            }
-
-                                        </div>
-                                    </div>
-
-                                    <br />
-                                    <br />
-                                    <p className='footerText'>Added by: {recipe.authorName}</p>
-                                </div>
-                            </div>
+                            displayRecipeCard(recipe, index)
                         ))
                 }
             </div>
